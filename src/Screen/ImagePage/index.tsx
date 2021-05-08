85a62ab07d7f0cc559b8ff165885bfab14ac8e57
onurrahmi <onurrahmi74@gmail.com>
2021-05-08 17:41:50 +0300

    image screen

--- src/Screen/ImagePage/index.tsx
+++ src/Screen/ImagePage/index.tsx
@@ -1,8 +1,64 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+  Text,
+  ViewBase,
+} from 'react-native';
+import IconI from 'react-native-vector-icons/Ionicons';
+import IconMI from 'react-native-vector-icons/MaterialIcons';
 import Styles from './styles';
 const index = () => {
-  return <SafeAreaView style={Styles.container}></SafeAreaView>;
+  return (
+    <SafeAreaView style={Styles.container}>
+      <View style={Styles.imageContainer}>
+        <ScrollView pagingEnabled style={Styles.flex} horizontal>
+          <Image
+            resizeMode="contain"
+            style={Styles.image}
+            source={{
+              uri:
+                'https://images.unsplash.com/photo-1516767784670-cfc730ed0f87?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fHByb2ZpbGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+            }}
+          />
+          <Image
+            resizeMode="contain"
+            style={Styles.image}
+            source={{
+              uri:
+                'https://images.unsplash.com/photo-1474978528675-4a50a4508dc3?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHByb2ZpbGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+            }}
+          />
+        </ScrollView>
+      </View>
+      <View style={Styles.closeContainer}>
+        <TouchableOpacity>
+          <View style={Styles.closeButton}>
+            <Text style={Styles.closeText}>X</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+      <View style={Styles.bottomContainer}>
+        <View style={Styles.contactContainer}>
+          <TouchableOpacity style={Styles.phoneButton}>
+            <IconMI name="phone" size={25} color="white" />
+          </TouchableOpacity>
+          <View style={Styles.space} />
+          <TouchableOpacity style={Styles.messageButton}>
+            <IconI name="paper-plane" size={25} color="white" />
+          </TouchableOpacity>
+        </View>
+        <View style={Styles.shareContainer}>
+          <TouchableOpacity style={Styles.shareButton}>
+            <IconI name="share-social" size={25} />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 };
 
 export default index;
--- src/Screen/ImagePage/styles.ts
+++ src/Screen/ImagePage/styles.ts
@@ -1,8 +1,80 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Dimensions} from 'react-native';
+const {width, height} = Dimensions.get('window');
 
 const Styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'black',
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    width: width,
+    height: height,
+  },
+  flex: {
+    flex: 1,
+  },
+  closeContainer: {
+    position: 'absolute',
+    left: 0,
+    top: 0,
+  },
+  closeButton: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: 'gray',
+    marginLeft: 10,
+    marginTop: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  closeText: {
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  bottomContainer: {
+    width: width,
+    position: 'absolute',
+    bottom: 0,
+  },
+  shareContainer: {
+    width: '100%',
+    alignItems: 'center',
+  },
+  shareButton: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  phoneButton: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: '#6EF60A',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  messageButton: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: '#0AC4F6',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  contactContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  space: {
+    width: 50,
   },
 });
 
